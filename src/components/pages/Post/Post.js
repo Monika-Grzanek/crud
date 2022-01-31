c542712e11dea188a6a24a0756135fed3ca0e165
Monika Grzanek <monski@o2.pl>
2022-01-31 18:47:19 +0100

    Render single post and add buttons to edit and remove in Post.js

--- src/components/pages/Post/Post.js
+++ src/components/pages/Post/Post.js
@@ -1,7 +1,32 @@
+import { useParams } from 'react-router';
+import { getPostById } from './../../../redux/postsRedux';
+import { useSelector } from 'react-redux';
+import { Navigate } from 'react-router-dom';
+import { Button, Card } from "react-bootstrap";
+import {NavLink} from 'react-router-dom';
+
 
 const Post = () => {
+    const {id} = useParams();
+    const postData = useSelector(state => getPostById(state, id));
+    if(!postData) return <Navigate to="/" />
     return (
-        <h1>Single Post</h1>
+        <div className="posts-navi d-flex justify-content-around" >
+            <Card style={{border: 'white'}} >
+                <Card.Body>
+                <Card.Title>{postData.title}</Card.Title>
+                <Card.Text>
+                    <p><b>Author: </b>{postData.author}</p>
+                    <p><b>Published: </b>{postData.publishedDate}</p>
+                    <p>{postData.content}</p>
+                </Card.Text>
+                </Card.Body>
+            </Card>
+            <div>
+                <Button as={NavLink} to={`/post/edit/${id}`} size="sm" variant="outline-info">Edit</Button>{' '}
+                <Button as={NavLink} to="/post/add" size="sm" variant="outline-danger">Remove</Button>
+            </div>
+        </div>
     )
 }
 
