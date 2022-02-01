31cd6d9c821adc1f2463eee56aa7f329b072fecb
Monika Grzanek <monski@o2.pl>
2022-02-01 13:42:30 +0100

    Add function removePost in ModalPage.js

--- src/components/features/ModalPage.js
+++ src/components/features/ModalPage.js
@@ -1,16 +1,22 @@
 import { Modal, Button } from "react-bootstrap";
 import { useState } from "react";
+import { useDispatch } from 'react-redux';
+import { removePost } from "./../../redux/postsRedux";
 
-const ModalPage = (props) => {
+const ModalPage = ({id}) => {
     const [show, setShow] = useState(false);
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
-
+    const dispatch = useDispatch();
+    const handleSubmit = e => {
+        e.preventDefault();
+        dispatch(removePost({id}));
+    }
     return (
         <>
             <Button onClick={handleShow} size="sm" variant="outline-danger">Remove</Button>
             <Modal show={show} onHide={handleClose} backdrop='static' keyboard={false}>
-                <Modal.Header closeButton>
+                <Modal.Header closeButton >
                     <Modal.Title>Are you sure?</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
@@ -21,7 +27,7 @@ const ModalPage = (props) => {
                     <Button variant='secondary' onClick={handleClose}>
                         Cancel
                     </Button>
-                    <Button variant='danger' onClick={handleClose}>
+                    <Button variant='danger' onClick={handleSubmit}>
                         Remove
                     </Button>
                 </Modal.Footer>
--- src/components/pages/Post/Post.js
+++ src/components/pages/Post/Post.js
@@ -2,7 +2,7 @@ import { useParams } from 'react-router';
 import { getPostById } from './../../../redux/postsRedux';
 import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
-import { Button, Card, Row} from "react-bootstrap";
+import { Button, Card} from "react-bootstrap";
 import {Link} from 'react-router-dom';
 import ModalPage from './../../features/ModalPage.js';
 
@@ -24,7 +24,7 @@ const Post = () => {
             </Card>
             <div>
                 <Button as={Link} to={`/post/edit/${id}`} size="sm" variant="outline-info" >Edit</Button>{' '}
-                <ModalPage />
+                <ModalPage id={postData.id}/>
             </div>
         </div>
     )
