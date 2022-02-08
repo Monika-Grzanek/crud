d0f2127fdf518f196622292146e398a219b5fd6b
Monika Grzanek <monski@o2.pl>
2022-02-08 12:18:50 +0100

    Add header in Category.js

--- src/components/pages/Category/Category.js
+++ src/components/pages/Category/Category.js
@@ -1,6 +1,9 @@
+import { useParams } from 'react-router';
+
 const Category = () => {
+    const {titleCat} = useParams();
     return (
-        <h1>Category:</h1>
+        <h1>Category: {titleCat}</h1>
     )
 }
 
